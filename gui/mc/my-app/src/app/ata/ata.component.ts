d287bb86dff7df1ccb840b2fb6d5c35e2924ff7a
VinnyAmbesek <vinnyrpgmeister@gmail.com>
2017-11-27 14:14:13 -0300

    Base screen.

---
+++ gui/mc/my-app/src/app/ata/ata.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ata',
+  templateUrl: './ata.component.html',
+  styleUrls: ['./ata.component.css']
+})
+export class AtaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
